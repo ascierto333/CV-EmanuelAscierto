fb70ae8c055fd22686bbe37b0d11fa45da8795c2
ascierto333 <123520476+ascierto333@users.noreply.github.com>
2023-03-08 22:42:20 -0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,12 @@
+function alertaface() {
+  alert('saludos desde mi facebook');
+}
+
+function alertainsta() {
+    alert('perdón si no se puede ver por que mi insta está en privado');
+  }
+
+function alertamail() {
+    alert('me vas a tener que enviar un mail con oferta laboral!!!');
+  }
+
