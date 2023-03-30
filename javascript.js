b1c9b05baf1b8ce14d67098b25a5aecb3a385c7c
ascierto333 <123520476+ascierto333@users.noreply.github.com>
2023-03-30 19:51:19 -0300

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -10,3 +10,12 @@ function alertamail() {
     alert('me vas a tener que enviar un mail con oferta laboral!!!');
   }
 
+function mostrarDatos() {
+  var datos = document.getElementById("datos");
+  if (datos.style.display === "none") {
+    datos.style.display = "block";
+  } else {
+    datos.style.display = "none";
+  }
+}
+
